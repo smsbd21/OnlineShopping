49b08f99935f2a5761097cfff38fec8b332386b9
Sumon Mahbub Shah <smsbd9@hotmail.com>
2021-10-07 23:51:12 +0600

    Add project files.

---
+++ OnlineShop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Areas.Admin
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        ApplicationDbContext db;
+        public ProductTypesController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.ProductTypes.ToList());
+        }
+        //Create Get Action Method
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //Create Post Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if(ModelState.IsValid)
+            {
+                db.ProductTypes.Add(productTypes);
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+    }
+}
---
+++ OnlineShop/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+
+namespace OnlineShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    //webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.ConfigureHttpsDefaults(co =>
+                        {
+                            co.SslProtocols = SslProtocols.Tls12;
+                        });
+                    }).UseStartup<Startup>();
+                });
+    }
+}
